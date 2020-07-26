2b0a7203d85957d3de51b9e11f257eaa06653155
Ihor Dotsenko <igordocenko1997@gmail.com>
2020-07-26 15:10:12 +0300

    Migrated from d3.js to canvas. Added an ability to zoom chart(s) (#1)
    * Added an ability to zoom chart(s)
    
    * Renamed xBand to xScale to keep code consistent
    
    * Tuned TS config to complain about unused variables
    
    * Upgraded type definitions for node and papaparse
    
    * Migrated from d3.js (SVG) to canvas for drawing OHLC bars
    
    * Migrated to OOP approach for rendering and tried to improve UX of zooming
    
    * Got rid of useless patch-package
    
    * Reimplemented drawing and zooming completely
    
    * Prettified and refactored the code around drawing and zooming

--- src/components/OHLCChart.tsx
+++ src/components/OHLCChart.tsx
@@ -1,95 +1,183 @@
 import React from "react";
 import * as d3 from "d3";
 
-const MARGINS = { top: 10, right: 15, bottom: 20, left: 35 };
+const INITIAL_VISIBLE_CANDLES_COUNT = 150;
+const ZOOM_BASE_COEFFICIENT = 0.999;
 
-const OHLCChart = ({ name: title, records }: OHLCFile) => {
-  const containerRef = React.useRef<SVGSVGElement>(null);
+const ohlcRecordToColor = (record: OHLCRecord): string => {
+  if (record.Open === record.Close) return "silver";
+  return record.Open > record.Close ? "red" : "green";
+};
+
+class CanvasDrawer {
+  static readonly GET_CONTEXT_2D_FAIL_MSG =
+    "Getting of 2D Context failed... May your browser not support it?..";
+
+  static readonly CONTEXT_2D_MISSING_MSG =
+    "2D Context is missing... I suppose you forget to call #prepare...";
+
+  private canvas: HTMLCanvasElement;
+  private context?: CanvasRenderingContext2D;
+
+  private width: number;
+  private height: number;
+
+  private records: readonly OHLCRecord[];
+
+  private yScale: any;
+  private xScale: any;
+
+  private candleStickWidth = 0;
+
+  constructor(
+    canvas: HTMLCanvasElement,
+    width: number,
+    height: number,
+    records: OHLCRecord[]
+  ) {
+    this.canvas = canvas;
+
+    this.width = width;
+    this.height = height;
+
+    this.records = records;
+
+    this.yScale = d3.scaleLinear().range([height, 0]);
+    this.xScale = d3.scaleLinear().range([0, width]);
+  }
+
+  prepare(scale = window.devicePixelRatio) {
+    this.canvas.width = this.width * scale;
+    this.canvas.height = this.height * scale;
+
+    this.canvas.style.width = `${this.width}px`;
+    this.canvas.style.height = `${this.height}px`;
+
+    const context = this.canvas.getContext("2d");
+
+    if (context) {
+      this.context = context;
+      this.context.scale(scale, scale);
+    } else {
+      throw CanvasDrawer.GET_CONTEXT_2D_FAIL_MSG;
+    }
+  }
+
+  draw(firstVisibleCandleIndex: number, lastVisibleCandleIndex: number) {
+    if (this.context == null) throw CanvasDrawer.CONTEXT_2D_MISSING_MSG;
+
+    const records = this.records.slice(
+      Math.max(Math.floor(firstVisibleCandleIndex), 0),
+      Math.min(Math.ceil(lastVisibleCandleIndex + 1), this.records.length)
+    );
+
+    this.yScale.domain([
+      d3.min(records.map((record) => record.Low))!,
+      d3.max(records.map((record) => record.High))!,
+    ]);
+
+    this.xScale.domain([firstVisibleCandleIndex, lastVisibleCandleIndex + 1]);
+
+    this.candleStickWidth =
+      this.width / (lastVisibleCandleIndex - firstVisibleCandleIndex + 1);
+
+    this.context.clearRect(0, 0, this.width, this.height);
+
+    records.forEach((record, i) => {
+      this.drawCandleStick(
+        record,
+        i + Math.max(Math.floor(firstVisibleCandleIndex), 0)
+      );
+    });
+  }
+
+  private drawCandleStick(record: OHLCRecord, i: number) {
+    this.drawStick(record, i);
+    this.drawCandle(record, i);
+  }
+
+  private drawStick(record: OHLCRecord, i: number) {
+    if (this.context == null) throw CanvasDrawer.CONTEXT_2D_MISSING_MSG;
+
+    this.context.beginPath();
+
+    this.context.moveTo(
+      this.xScale(i) + this.candleStickWidth / 2,
+      this.yScale(Math.max(record.High, record.Low))
+    );
+
+    this.context.lineTo(
+      this.xScale(i) + this.candleStickWidth / 2,
+      this.yScale(Math.min(record.High, record.Low))
+    );
+
+    this.context.strokeStyle = ohlcRecordToColor(record);
+
+    this.context.stroke();
+  }
+
+  private drawCandle(record: OHLCRecord, i: number) {
+    if (this.context == null) throw CanvasDrawer.CONTEXT_2D_MISSING_MSG;
+
+    this.context.beginPath();
+
+    this.context.rect(
+      this.xScale(i),
+      this.yScale(Math.max(record.Open, record.Close)),
+      this.candleStickWidth,
+      record.Open === record.Close
+        ? 1
+        : this.yScale(Math.min(record.Open, record.Close)) -
+            this.yScale(Math.max(record.Open, record.Close))
+    );
+
+    this.context.fillStyle = ohlcRecordToColor(record);
+    this.context.fill();
+
+    this.context.strokeStyle = "black";
+    this.context.stroke();
+  }
+}
+
+const OHLCChart = ({ records }: OHLCFile) => {
+  const canvasRef = React.useRef<HTMLCanvasElement>(null);
+  const containerRef = React.useRef<HTMLDivElement>(null);
 
   React.useEffect(() => {
     const container = containerRef.current!;
+    const canvas = canvasRef.current!;
 
-    const width = container.clientWidth - MARGINS.left - MARGINS.right;
-    const height = container.clientHeight - MARGINS.top - MARGINS.bottom;
-
-    const svg = d3
-      .select(container)
-      .append("g")
-      .attr("transform", `translate(${MARGINS.left}, ${MARGINS.top})`);
-
-    const timestamps = records.map(
-      (record) => new Date(record.Timestamp * 1e3)
+    const canvasDrawer = new CanvasDrawer(
+      canvas,
+      container.clientWidth,
+      container.clientHeight,
+      records
     );
 
-    const xBand = d3
-      .scaleBand<Date>()
-      .domain(timestamps)
-      .range([0, width])
-      .paddingInner(0.1)
-      .paddingOuter(1)
-      .align(0.5);
+    canvasDrawer.prepare();
+    canvasDrawer.draw(0, INITIAL_VISIBLE_CANDLES_COUNT);
 
-    const xAxis = d3.axisBottom<Date>(xBand).tickValues([timestamps[25]]);
+    let zoomCoefficient = 1;
 
-    svg
-      .append("g")
-      .attr("class", "x-axis")
-      .attr("transform", `translate(0, ${height})`)
-      .call(xAxis);
+    canvas.addEventListener("wheel", (event) => {
+      event.preventDefault();
 
-    const yScale = d3
-      .scaleLinear()
-      .range([height, 0])
-      .domain([
-        d3.min(records.map((record) => record.Low))!,
-        d3.max(records.map((record) => record.High))!,
-      ])
-      .nice();
+      zoomCoefficient *= ZOOM_BASE_COEFFICIENT ** event.deltaY;
 
-    const yAxis = d3.axisLeft<number>(yScale);
+      const deltaVisibleCandlesCount =
+        INITIAL_VISIBLE_CANDLES_COUNT * (zoomCoefficient - 1);
 
-    svg.append("g").attr("class", "y-axis").call(yAxis);
-
-    const candlesContainer = svg.append("g").attr("class", "candles-container");
-
-    // TODO: merge rect.candle and g.whisker into one
-
-    candlesContainer
-      .selectAll("rect.candle")
-      .data(records)
-      .enter()
-      .append("rect")
-      .attr("x", (_, i) => xBand(timestamps[i])!)
-      .attr("class", "candle")
-      .attr("y", (d) => yScale(Math.max(d.Open, d.Close)))
-      .attr("width", xBand.bandwidth())
-      .attr("height", (d) =>
-        d.Open === d.Close
-          ? 1
-          : yScale(Math.min(d.Open, d.Close)) -
-            yScale(Math.max(d.Open, d.Close))
-      )
-      .attr("fill", (d) =>
-        d.Open === d.Close ? "silver" : d.Open > d.Close ? "red" : "green"
+      canvasDrawer.draw(
+        -deltaVisibleCandlesCount / 2,
+        +deltaVisibleCandlesCount / 2 + INITIAL_VISIBLE_CANDLES_COUNT
       );
-
-    candlesContainer
-      .selectAll("g.whisker")
-      .data(records)
-      .enter()
-      .append("line")
-      .attr("class", "whisker")
-      .attr("x1", (_, i) => xBand(timestamps[i])! + xBand.bandwidth() / 2)
-      .attr("x2", (_, i) => xBand(timestamps[i])! + xBand.bandwidth() / 2)
-      .attr("y1", (d) => yScale(d.High))
-      .attr("y2", (d) => yScale(d.Low))
-      .attr("stroke", (d) =>
-        d.Open === d.Close ? "white" : d.Open > d.Close ? "red" : "green"
-      );
+    });
   }, [records]);
 
   return (
-    <svg className="d3-container" ref={containerRef} style={{ flex: 1 }} />
+    <div ref={containerRef} style={{ background: "lightgray", flex: 1 }}>
+      <canvas ref={canvasRef} />
+    </div>
   );
 };
 
