b310be5a9cafd4cf1cb85a258185ed50eca3da11
Ihor Dotsenko <igordocenko1997@gmail.com>
2020-07-26 08:08:39 +0300

    Migrated to OOP approach for rendering and tried to improve UX of zooming

--- src/components/OHLCChart.tsx
+++ src/components/OHLCChart.tsx
@@ -1,13 +1,165 @@
 import React from "react";
 import * as d3 from "d3";
 
-const CANDLES_INNER_PADDING = 0.5;
-let VISIBLE_CANDLES_COUNT = 10;
+const INITIAL_FIRST_VISIBLE_CANDLE_INDEX = 0;
+const INITIAL_LAST_VISIBLE_CANDLE_INDEX = 10;
+
+const INITIAL_VISIBLE_CANDLES_COUNT =
+  INITIAL_LAST_VISIBLE_CANDLE_INDEX - INITIAL_FIRST_VISIBLE_CANDLE_INDEX;
+
+const SCROLL_BASE_COEFFICIENT = 0.999;
+const SPACE_BETWEEN_CANDLES = 0.5;
 
 const ohlcRecordToColor = (record: OHLCRecord): string => {
   if (record.Open === record.Close) return "silver";
   return record.Open > record.Close ? "red" : "green";
 };
+
+class CanvasDrawer {
+  static readonly GET_CONTEXT_2D_FAIL_MSG =
+    "Getting of 2D Context failed... May your browser not support it?..";
+
+  static readonly CONTEXT_2D_MISSING_MSG =
+    "2D Context is missing... I suppose you forget to call #prepare...";
+
+  private canvas: HTMLCanvasElement;
+  private context?: CanvasRenderingContext2D;
+
+  private width: number;
+  private height: number;
+
+  private records: readonly OHLCRecord[];
+
+  private yScale: any;
+
+  constructor(
+    canvas: HTMLCanvasElement,
+    width: number,
+    height: number,
+    records: OHLCRecord[]
+  ) {
+    this.canvas = canvas;
+
+    this.width = width;
+    this.height = height;
+
+    this.records = records;
+
+    this.yScale = d3.scaleLinear().range([height, 0]).nice();
+  }
+
+  prepare(scale = window.devicePixelRatio) {
+    this.canvas.width = this.width * scale;
+    this.canvas.height = this.height * scale;
+
+    this.canvas.style.width = `${this.width}px`;
+    this.canvas.style.height = `${this.height}px`;
+
+    const context = this.canvas.getContext("2d");
+
+    if (context) {
+      this.context = context;
+      this.context.scale(scale, scale);
+    } else {
+      throw CanvasDrawer.GET_CONTEXT_2D_FAIL_MSG;
+    }
+  }
+
+  draw(
+    candleWidth: number,
+    firstVisibleCandleIndex: number,
+    lastVisibleCandleIndex: number,
+    offsetLeft: number = 0
+  ) {
+    if (this.context == null) throw CanvasDrawer.CONTEXT_2D_MISSING_MSG;
+
+    const records = this.records.slice(
+      firstVisibleCandleIndex < 0 ? 0 : firstVisibleCandleIndex,
+      lastVisibleCandleIndex
+    );
+
+    this.yScale.domain([
+      d3.min(records.map((record) => record.Low))!,
+      d3.max(records.map((record) => record.High))!,
+    ]);
+
+    this.context.clearRect(0, 0, this.width, this.height);
+
+    records.forEach((record, i) => {
+      this.drawCandleStick(record, i, candleWidth, offsetLeft);
+    });
+  }
+
+  private drawCandleStick(
+    record: OHLCRecord,
+    i: number,
+    candleWidth: number,
+    offsetLeft: number
+  ) {
+    const color = ohlcRecordToColor(record);
+
+    this.drawStick(record, i, candleWidth, color, offsetLeft);
+    this.drawCandle(record, i, candleWidth, color, offsetLeft);
+  }
+
+  private drawStick(
+    record: OHLCRecord,
+    i: number,
+    candleWidth: number,
+    color: string,
+    offsetLeft: number
+  ) {
+    if (this.context == null) throw CanvasDrawer.CONTEXT_2D_MISSING_MSG;
+
+    this.context.beginPath();
+
+    this.context.moveTo(
+      offsetLeft +
+        i * candleWidth * (1 + SPACE_BETWEEN_CANDLES) +
+        candleWidth / 2,
+      this.yScale(Math.max(record.High, record.Low))
+    );
+
+    this.context.lineTo(
+      offsetLeft +
+        i * candleWidth * (1 + SPACE_BETWEEN_CANDLES) +
+        candleWidth / 2,
+      this.yScale(Math.min(record.High, record.Low))
+    );
+
+    this.context.strokeStyle = color;
+
+    this.context.stroke();
+  }
+
+  private drawCandle(
+    record: OHLCRecord,
+    i: number,
+    candleWidth: number,
+    color: string,
+    offsetLeft: number
+  ) {
+    if (this.context == null) throw CanvasDrawer.CONTEXT_2D_MISSING_MSG;
+
+    this.context.beginPath();
+
+    this.context.rect(
+      offsetLeft + i * candleWidth * (1 + SPACE_BETWEEN_CANDLES),
+      this.yScale(Math.max(record.Open, record.Close)),
+      candleWidth,
+      record.Open === record.Close
+        ? 1
+        : this.yScale(Math.min(record.Open, record.Close)) -
+            this.yScale(Math.max(record.Open, record.Close))
+    );
+
+    this.context.fillStyle = color;
+    this.context.fill();
+
+    this.context.strokeStyle = "black";
+    this.context.stroke();
+  }
+}
 
 const OHLCChart = ({ records }: OHLCFile) => {
   const canvasRef = React.useRef<HTMLCanvasElement>(null);
@@ -19,85 +171,63 @@ const OHLCChart = ({ records }: OHLCFile) => {
     const height = container.clientHeight;
 
     const canvas = canvasRef.current!;
-    canvas.width = width * window.devicePixelRatio;
-    canvas.height = height * window.devicePixelRatio;
-    canvas.style.width = `${width}px`;
-    canvas.style.height = `${height}px`;
 
-    const context = canvas.getContext("2d")!;
-    context.scale(window.devicePixelRatio, window.devicePixelRatio);
-
-    const yScale = d3
-      .scaleLinear()
-      .range([height, 0])
-      .domain([
-        d3.min(records.map((record) => record.Low))!,
-        d3.max(records.map((record) => record.High))!,
-      ])
-      .nice();
-
-    // `ww` is for `window width`
-    // `cw` is for `candle width`
-    // `cp` is for `candle padding`
-    // ww - 1 = N * cw + (N - 1) * cp * cw
-    // ww - 1 = (N + (N - 1) * cp) * cw
-    // cw = (w - 1) / (N + (N - 1) * cp)
-    let bandwidth =
+    const initialCandleWidth =
       (width - 1) /
-      (VISIBLE_CANDLES_COUNT +
-        (VISIBLE_CANDLES_COUNT - 1) * CANDLES_INNER_PADDING);
+      (INITIAL_VISIBLE_CANDLES_COUNT +
+        (INITIAL_VISIBLE_CANDLES_COUNT - 1) * SPACE_BETWEEN_CANDLES);
 
-    let candlesCount = VISIBLE_CANDLES_COUNT;
+    let zoomCoefficient = 1,
+      deltaWidthLeft = 0,
+      deltaWidthRight = 0;
 
-    const draw = () => {
-      context.clearRect(0, 0, width, height);
+    const canvasDrawer = new CanvasDrawer(canvas, width, height, records);
 
-      records.slice(0, candlesCount).forEach((record, i) => {
-        context.beginPath();
+    canvasDrawer.prepare();
 
-        context.moveTo(
-          i * bandwidth * (1 + CANDLES_INNER_PADDING) + bandwidth / 2,
-          yScale(Math.max(record.High, record.Low))
-        );
-
-        context.lineTo(
-          i * bandwidth * (1 + CANDLES_INNER_PADDING) + bandwidth / 2,
-          yScale(Math.min(record.High, record.Low))
-        );
-
-        context.strokeStyle = ohlcRecordToColor(record);
-
-        context.stroke();
-
-        context.beginPath();
-
-        context.rect(
-          i * bandwidth * (1 + CANDLES_INNER_PADDING),
-          yScale(Math.max(record.Open, record.Close)),
-          bandwidth,
-          record.Open === record.Close
-            ? 1
-            : yScale(Math.min(record.Open, record.Close)) -
-                yScale(Math.max(record.Open, record.Close))
-        );
-
-        context.fillStyle = ohlcRecordToColor(record);
-        context.fill();
-
-        context.strokeStyle = "black";
-        context.stroke();
-      });
-    };
-
-    draw();
+    canvasDrawer.draw(
+      initialCandleWidth,
+      INITIAL_FIRST_VISIBLE_CANDLE_INDEX,
+      INITIAL_LAST_VISIBLE_CANDLE_INDEX
+    );
 
     canvas.addEventListener("wheel", (e) => {
       e.preventDefault();
 
-      bandwidth = bandwidth * 0.99 ** e.deltaY;
-      candlesCount = Math.ceil((width - 1) / bandwidth);
+      const prevWidth = width * zoomCoefficient;
+      zoomCoefficient *= SCROLL_BASE_COEFFICIENT ** e.deltaY;
+      const currentWidth = width * zoomCoefficient;
 
-      draw();
+      const deltaWidth = prevWidth - currentWidth;
+      deltaWidthLeft += deltaWidth * (e.offsetX / width);
+      deltaWidthRight += deltaWidth * (1 - e.offsetX / width);
+
+      const candleWidth = initialCandleWidth * zoomCoefficient;
+
+      const deltaCandlesLeft = Math.ceil(
+        (deltaWidthLeft - candleWidth * SPACE_BETWEEN_CANDLES) /
+          (candleWidth * (1 + SPACE_BETWEEN_CANDLES))
+      );
+
+      const deltaCandlesRight = Math.ceil(
+        (deltaWidthRight - candleWidth * SPACE_BETWEEN_CANDLES) /
+          (candleWidth * (1 + SPACE_BETWEEN_CANDLES))
+      );
+
+      console.log(
+        INITIAL_FIRST_VISIBLE_CANDLE_INDEX - deltaCandlesLeft,
+        INITIAL_LAST_VISIBLE_CANDLE_INDEX + deltaCandlesRight
+      );
+
+      canvasDrawer.draw(
+        candleWidth,
+        INITIAL_FIRST_VISIBLE_CANDLE_INDEX - deltaCandlesLeft,
+        INITIAL_LAST_VISIBLE_CANDLE_INDEX + deltaCandlesRight,
+        deltaWidthLeft -
+          (deltaCandlesLeft < 0
+            ? deltaCandlesLeft * (candleWidth * (1 + SPACE_BETWEEN_CANDLES))
+            : 0)
+      );
     });
   }, [records]);
 
