d6d0146cf91078a98e5dd58699f5017b46bae4f0
Ihor Dotsenko <igordocenko1997@gmail.com>
2021-10-21 20:28:26 +0300

    Migrated from mouse to keyboard events for chart traveling

--- src/components/CandlesticksViewPoint.tsx
+++ src/components/CandlesticksViewPoint.tsx
@@ -1,18 +1,46 @@
-import { ReactNode, useMemo } from "react";
+import { ReactNode, useEffect, useMemo, useState } from "react";
 
 import CandlesticksViewPointContext from "contexts/CandlesticksViewPoint";
 
 import useNamedCandlesticks from "hooks/high-level/useNamedCandlesticks";
 import useCandlesticksSettings from "hooks/high-level/useCandlesticksSettings";
 
+import usePixiApplication from "hooks/high-level/usePixiApplication";
 import usePixiDimensions from "hooks/high-level/usePixiDimensions";
 
 const CandlesticksViewPoint = (props: { children: ReactNode }) => {
   const { candlesticks } = useNamedCandlesticks();
 
+  const app = usePixiApplication();
   const { width } = usePixiDimensions();
   const { candlestickPlaceholderWidth } = useCandlesticksSettings();
   const maxVisibleCandlesticksCount = width / candlestickPlaceholderWidth;
+
+  const [firstVisibleCandlestickIndex, setFirstVisibleCandlestickIndex] =
+    useState(0);
+
+  useEffect(() => {
+    const type = "keydown";
+
+    const listener = (e: KeyboardEvent) => {
+      setFirstVisibleCandlestickIndex((firstVisibleCandlestickIndex) => {
+        switch (e.code) {
+          case "ArrowRight":
+            return firstVisibleCandlestickIndex + 15;
+          case "ArrowLeft":
+            return firstVisibleCandlestickIndex - 15;
+          default:
+            return firstVisibleCandlestickIndex;
+        }
+      });
+    };
+
+    window.addEventListener(type, listener);
+
+    return () => {
+      window.removeEventListener(type, listener);
+    };
+  }, [app]);
 
   const candlesticksViewPoint = useMemo(() => {
     const visibleCandlesticksCount = Math.min(
@@ -20,7 +48,6 @@ const CandlesticksViewPoint = (props: { children: ReactNode }) => {
       candlesticks.length
     );
 
-    const firstVisibleCandlestickIndex = 0;
     const lastVisibleCandlestickIndex =
       firstVisibleCandlestickIndex + visibleCandlesticksCount;
 
@@ -30,7 +57,7 @@ const CandlesticksViewPoint = (props: { children: ReactNode }) => {
       //
       maxVisibleCandlesticksCount,
     };
-  }, [maxVisibleCandlesticksCount, candlesticks]);
+  }, [candlesticks, firstVisibleCandlestickIndex, maxVisibleCandlesticksCount]);
 
   return (
     <CandlesticksViewPointContext.Provider value={candlesticksViewPoint}>
