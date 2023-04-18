0a4acf84c407b7b3d0a122fa8f912d516d917561
Ihor Dotsenko <igordocenko1997@gmail.com>
2023-04-18 12:54:51 +0300

    Upgraded `@rspack/cli` (#479)
    * Upgraded `@rspack/cli` to `0.1.8`
    
    * Revert "Fixed broken deploy CI flow (#478)"
    
    This reverts commit 3eeddd871277bfed78ff254ca19b5b79b98b0d07.
    
    * Revert "Fixed broken deploy CI flow (#476)"
    
    This reverts commit f39412266e954dbbd388a5b975cb7dca584d9783.

--- rspack.config.js
+++ rspack.config.js
@@ -1,13 +1,11 @@
 const path = require("path");
 
-// https://github.com/web-infra-dev/rspack/issues/2616
-const PUBLIC_URL = ["build", "production"].includes(process.env.NODE_ENV)
-  ? "/financial-time-series-labeler"
-  : "";
+const PUBLIC_URL =
+  process.env.NODE_ENV === "production" ? "/financial-time-series-labeler" : "";
 
 /** @type {import('@rspack/cli').Configuration} */
 module.exports = {
-  target: "web", // https://github.com/web-infra-dev/rspack/issues/2735
+  target: "browserslist",
   entry: "./src/index.tsx",
   output: { clean: true, filename: "[name].[contenthash].js" },
   resolve: {
