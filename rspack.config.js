90477b75acb4c857f0363c5a00cec0ca01cc434f
Ihor Dotsenko <igordocenko1997@gmail.com>
2023-04-18 09:50:30 +0000

    Revert "Fixed broken deploy CI flow (#478)"
    This reverts commit 3eeddd871277bfed78ff254ca19b5b79b98b0d07.

--- rspack.config.js
+++ rspack.config.js
@@ -1,9 +1,7 @@
 const path = require("path");
 
-// https://github.com/web-infra-dev/rspack/issues/2616
-const PUBLIC_URL = ["build", "production"].includes(process.env.NODE_ENV)
-  ? "/financial-time-series-labeler"
-  : "";
+const PUBLIC_URL =
+  process.env.NODE_ENV === "production" ? "/financial-time-series-labeler" : "";
 
 /** @type {import('@rspack/cli').Configuration} */
 module.exports = {
